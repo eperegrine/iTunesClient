1bb1e27462fa566c7860596ab5e209204504f70f
Emily Peregrine <peregrineemily@gmail.com>
2018-09-18 11:43:25 +0100

    Initial Commit

---
+++ iTunesClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTunesClient
+//
+//  Created by Emily Peregrine on 18/09/2018.
+//  Copyright © 2018 Emily Peregrine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
